0ef93792bbac7cfa6044a1a1014ee2e47950d781
Zelipha <zeliphawambui756@gmail.com>
2021-11-24 13:49:59 -0800

    A shorter approach of the same function

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique = []
-    for number in my_list:
-        if number in unique:
-            continue
-        else:
-            unique.append(number)
+    unique = list(set(my_list))
     return sum(unique)
