19b3a173bb18ec24744094609433b52809a0b5a5
Zelipha <zeliphawambui756@gmail.com>
2021-12-06 08:54:24 -0800

    Safe integer print with error message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as err:
+        print("Exception:", err)
+        return False
+    except TypeError:
+        return False
