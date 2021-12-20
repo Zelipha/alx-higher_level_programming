551176dae5a613de8c484505a29b647598e002f2
Zelipha <zeliphawambui756@gmail.com>
2021-12-20 14:24:29 -0800

    Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -16,6 +16,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """Represents a class Rectangle that inherits from the base class"""
     def __init__(self, width, height):
