4b795c38a34e9a0f04195ce715cec39007d76cbc
Zelipha <zeliphawambui756@gmail.com>
2021-12-20 13:42:13 -0800

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class BaseGeometry with a public instance method"""
+
+
+class BaseGeometry:
+    """Represents base geometry"""
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
