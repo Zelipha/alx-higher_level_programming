e1f2df20790f8187a362687dd5f0b8cf40e9e55d
Zelipha <zeliphawambui756@gmail.com>
2021-12-21 12:00:06 -0800

    Function that appends a string

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that appends a string at the end of a txt file"""
+
+
+def append_write(filename="", text=""):
+    """Function that appends a string at the end of a text file (UTF8) and
+        returns the number of characters added.
+    """
+
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
