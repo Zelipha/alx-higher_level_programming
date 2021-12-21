54e8da8af264eb66f84c10927bcc409815631fe3
Zelipha <zeliphawambui756@gmail.com>
2021-12-21 11:20:43 -0800

    Function that reads a text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that reads a text file"""
+
+
+def read_file(filename=""):
+    """Function that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
