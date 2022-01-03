cf52895273c474e85d887bc5e6f34fecabb65bd9
Zelipha <zeliphawambui756@gmail.com>
2022-01-03 06:02:38 -0800

    Comparison operators

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class MyInt"""
+
+
+class MyInt(int):
+    """MyInt inherits from int. MyInt has == and != operators inverted"""
+    
+    def __eq__(self, value):
+        """Override == opeartor with != behavior"""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior"""
+        return self.real == value
