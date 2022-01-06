324b478326cd3544e2e3e37888febdbc0e9b7f90
Zelipha <zeliphawambui756@gmail.com>
2022-01-06 11:29:23 -0800

    Fixed pep8 error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """Represents a class Rectangle that inherits from class Base"""
-    
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes the rectangle"""
         Base.__init__(self, id)
