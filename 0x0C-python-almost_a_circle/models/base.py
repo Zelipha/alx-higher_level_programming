def16bbcd23aab83db25720d48c1e13320a3c177
Zelipha <zeliphawambui756@gmail.com>
2022-01-07 06:21:55 -0800

    Update base model file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,18 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Class method that writes the JSON string representation of
+            list_objs to a file
+        """
+        file_name = "{}.json".format(cls.__name__)
+        with open(file_name, 'w', encoding='utf-8') as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                new_list = []
+                for i in list_objs:
+                    new_list.append(i.to_dictionary())
+                f.write(Base.to_json_string(new_list))
