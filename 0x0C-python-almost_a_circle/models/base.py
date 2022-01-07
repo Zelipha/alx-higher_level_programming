0cd90161f71674111e7d4faad576b05688746031
Zelipha <zeliphawambui756@gmail.com>
2022-01-07 06:38:40 -0800

    Update base model file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,5 +44,5 @@ class Base:
     def from_json_string(json_string):
         """Returns the list of the JSON string representation json_string"""
         if json_string is None or json_string == "":
-            return "[]"
+            return []
         return json.loads(json_string)
