78ce36ee23345cf88c112bce7687dfb9a1bd10fe
Zelipha <zeliphawambui756@gmail.com>
2022-02-02 13:15:05 -0800

    Creates a user and grants privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creates the MySQL server user user_0d_1
+-- user_0d_1 should have all privileges/ password should be set to user_0d_1_pwd
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
