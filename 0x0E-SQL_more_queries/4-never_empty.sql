b7c2dea73ae4f12711ac967e595588722cd8949e
Zelipha <zeliphawambui756@gmail.com>
2022-02-02 14:30:55 -0800

    Creates a table with default id value

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+-- Creates the table id_not_null
+-- id_not_null description: id INT with the default value 1, name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
