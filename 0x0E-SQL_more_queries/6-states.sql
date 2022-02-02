48edd643bbb26042244a9c1786daf3f2a1864072
Zelipha <zeliphawambui756@gmail.com>
2022-02-02 15:28:33 -0800

    Creates database and table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,9 +2,9 @@
 -- states description:
 --     id INT unique, auto generated, can’t be null and is a primary key
 --     name VARCHAR(256) can’t be null
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
-       PRIMARY KEY(`id`),
-       `id`   INT          NOT NULL AUTO_INCREMENT,
-       `name` VARCHAR(256) NOT NULL
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       PRIMARY KEY(id),
+       id   INT          NOT NULL AUTO_INCREMENT,
+       name VARCHAR(256) NOT NULL
        );
