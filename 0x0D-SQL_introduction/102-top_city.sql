728f8b9fea59131d4af3ce494b7ae6bd94f04d21
Zelipha <zeliphawambui756@gmail.com>
2022-02-03 07:54:09 -0800

    Displays top 3 cities

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
 -- Displays the top 3 of cities temperature during July and August ordered by temperature
-SELECT city AVG(value) AS avg_temp
+SELECT city, AVG(value) AS avg_temp
        FROM temperatures
        WHERE month = 7 || month = 8
        GROUP BY
