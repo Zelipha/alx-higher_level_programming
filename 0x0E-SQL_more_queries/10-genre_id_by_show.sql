d200777005e533701e560eb29429a57e03dd8837
Zelipha <zeliphawambui756@gmail.com>
2022-02-03 11:49:49 -0800

    Modified file

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-SELECT tv_shows.title, tv_show_genres.show_id
+SELECT tv_shows.title, tv_show_genres.genre_id
        FROM tv_show_genres
        INNER JOIN tv_shows
        ON tv_shows.id = tv_show_genres.show_id
