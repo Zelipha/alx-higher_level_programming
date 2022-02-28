76e70a2454abe81083254b3ed90958f8e9812126
Zelipha <zeliphawambui756@gmail.com>
2022-02-28 15:24:03 -0800

    Computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Computes and prints a factorial
+
+const num = parseInt(process.argv[2]);
+
+function factorial (num) {
+  if (isNaN(num) || num === 1) {
+    return 1;
+  } else {
+    return (num * factorial(num - 1));
+  }
+}
+console.log(factorial(num));
