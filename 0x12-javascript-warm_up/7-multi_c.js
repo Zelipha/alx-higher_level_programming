720bec84b2661c730dc4d224988cf63c17c34246
Zelipha <zeliphawambui756@gmail.com>
2022-02-28 14:52:26 -0800

    Prints x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const myNumber = parseInt(process.argv[2]);
+
+if (isNaN(myNumber)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < myNumber; i++) {
+    console.log('C is fun');
+  }
+}
