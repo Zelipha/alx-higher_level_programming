8e1a68ce5c91afde1d65549348e5147292b4c508
Zelipha <zeliphawambui756@gmail.com>
2022-02-28 14:44:36 -0800

    Prints an array using a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Prints 3 lines by using a loop
+
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
