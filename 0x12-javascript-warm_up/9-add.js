e2c9d75c83fa466d1c4fdc403d9b0ebf3a1ada12
Zelipha <zeliphawambui756@gmail.com>
2022-02-28 15:10:59 -0800

    Addition of 2 integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Prints the addition of 2 integers
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+function add (a, b) {
+  return a + b;
+}
+console.log(add(a, b));
