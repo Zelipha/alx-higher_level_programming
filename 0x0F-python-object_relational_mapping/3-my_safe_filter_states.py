f8f6c3473879ab076e6f1ef538a245c01ee3c41c
Zelipha <zeliphawambui756@gmail.com>
2022-03-10 03:38:24 -0800

    Safe from MySQL injections!

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Takes in arguments and displays all values in the states table of
+   hbtn_0e_0_usa where name matches the argument, safe from MySQL injections!
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+     db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+     cur = db.cursor()
+     cur.execute("SELECT * FROM states WHERE name =%s\
+                  ORDER BY states.id ASC", (sys.argv[4],))
+     rows = cur.fetchall()
+     for r in rows:
+          print(r)
