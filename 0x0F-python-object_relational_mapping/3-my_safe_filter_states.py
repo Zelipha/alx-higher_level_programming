2dcd299800cb3d90a99e1aade80333ac03fb011d
Zelipha <zeliphawambui756@gmail.com>
2022-03-10 03:42:47 -0800

    Fix pycodestyle

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,12 +8,12 @@ import sys
 
 if __name__ == '__main__':
 
-     db = MySQLdb.connect(host="localhost", port=3306,
+    db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-     cur = db.cursor()
-     cur.execute("SELECT * FROM states WHERE name =%s\
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name =%s\
                   ORDER BY states.id ASC", (sys.argv[4],))
-     rows = cur.fetchall()
-     for r in rows:
-          print(r)
+    rows = cur.fetchall()
+    for r in rows:
+        print(r)
