4bacfb8de86a76c964294ff35b322a69557e3836
Zelipha <zeliphawambui756@gmail.com>
2022-03-10 02:31:24 -0800

    Lists all states with a name starting with uppercase N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY\
