423fdae1beb8c564b367f764b813a7e8ce423888
Zelipha <zeliphawambui756@gmail.com>
2022-03-17 11:36:28 -0700

    MOdify file

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that takes in a URL and displays all HTTP methods the server will accept
-curl -sX "OPTIONS" "$1" -I | awk '/Allow/ {print $2}'
+curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
