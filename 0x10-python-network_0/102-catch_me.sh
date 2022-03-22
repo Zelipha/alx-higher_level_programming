ed69c78aa6840921626741ba64fd30adee8cbde2
Zelipha <zeliphawambui756@gmail.com>
2022-03-23 00:46:38 +0300

    Modify code

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message You got me!
-curl -sL -X PUT -d -H "Content-Type: text/html" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -d "user_id=98" -H "origin: HolbertonSchool" 0.0.0.0:5000/catch_me
