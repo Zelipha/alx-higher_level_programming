2ed9fef84f02bebc8acb4f54af87c4161f894d71
Zelipha <zeliphawambui756@gmail.com>
2022-04-12 20:48:19 +0300

    Script that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const file = process.argv[2];
 
 fs.readFile(file, 'utf8', (err, data) => {
-  if (err){
+  if (err) {
     console.log(err);
   } else {
     console.log(data);
