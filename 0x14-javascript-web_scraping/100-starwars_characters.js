2932d2d306a86119eccbdf195909d1ac600909f3
Zelipha <zeliphawambui756@gmail.com>
2022-04-12 23:40:51 +0300

    Prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// Script that prints all characters of a Star Wars movie 
+// Script that prints all characters of a Star Wars movie
 
 const request = require('request');
 const args = process.argv.slice(1);
