3ddba7a555249d1b78c00454f3c983254930748f
Zelipha <zeliphawambui756@gmail.com>
2022-04-12 23:57:22 +0300

    Modify file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 // Script that gets the contents of a webpage and stores it in a file
 
-const args = process.argv.slice(1);
 const request = require('request');
 const fs = require('fs');
-request(args[1]).pipe(fs.createWriteStream(args[2]));
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
