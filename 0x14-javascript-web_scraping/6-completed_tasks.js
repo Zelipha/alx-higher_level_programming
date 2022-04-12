6e208e905ee9f73bc1bf3af49de7e2b621c32adb
Zelipha <zeliphawambui756@gmail.com>
2022-04-12 23:45:24 +0300

    Computes the number of tasks completed by user id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// Script that computes the number of tasks completed by user id
+
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const dict = JSON.parse(body).reduce((acc, elem) => {
+    if (!acc[elem.userId]) {
+      if (elem.completed) {
+        acc[elem.userId] = 1;
+      }
+    } else {
+      if (elem.completed) {
+        acc[elem.userId] += 1;
+      }
+    }
+    return acc;
+  }, {});
+  console.log(dict);
+});
