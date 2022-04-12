598a25dbf56c67e4cf4a6f0667bccdff2003d87a
Zelipha <zeliphawambui756@gmail.com>
2022-04-12 23:32:19 +0300

    Prints the number of movies where a specific character is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,22 +2,12 @@
 // Script that prints the number of movies where the character “Wedge Antilles” is present
 
 const request = require('request');
-const args = process.argv.slice(1);
-request(args[1], (error, response, body) => {
-  let total = 0;
-  const actor = 'https://swapi-api.hbtn.io/api/people/18/';
+request(process.argv[2], function (error, response, body) {
   if (error) {
-    console.log(error);
-  } else {
-    const result = JSON.parse(body).results;
-    for (const res of result) {
-      const character = res.characters;
-      for (const car of character) {
-        if (car === actor) {
-          total = total + 1;
-        }
-      }
-    }
+    console.error(error);
   }
-  console.log(total);
+  const nb = JSON.parse(body).results.filter((elem) => {
+    return elem.characters.filter((url) => { return url.includes('18'); }).length;
+  }).length;
+  console.log(nb);
 });
