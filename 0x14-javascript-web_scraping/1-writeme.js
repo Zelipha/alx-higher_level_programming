3f7522a0722bc809d341fd5845291a12ed91ede2
Zelipha <zeliphawambui756@gmail.com>
2022-04-12 20:47:50 +0300

    Script that writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const file = process.argv[2];
 const string = process.argv[3];
 
-fs.writeFile (file, string, 'utf-8', err => {
+fs.writeFile(file, string, 'utf-8', err => {
   if (err) {
     console.log(err);
   }
