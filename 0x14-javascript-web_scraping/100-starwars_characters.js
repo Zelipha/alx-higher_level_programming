2298d52580f68d7cb7d3fbc9d56f5d574ff7312a
Zelipha <zeliphawambui756@gmail.com>
2022-04-12 23:39:49 +0300

    Prints all characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// Script that prints all characters of a Star Wars movie 
+
+const request = require('request');
+const args = process.argv.slice(1);
+const movie = 'https://swapi-api.hbtn.io/api/films/' + args[1];
+
+request(movie, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      request(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
